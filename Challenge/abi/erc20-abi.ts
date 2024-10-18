919bfbdfe318148fbe48f42503878c1311399134
XaViPanDx <83515045+XaViPanDx@users.noreply.github.com>
2024-10-18 17:14:36 +0200

    Create erc20-abi.ts

---
+++ Challenge/abi/erc20-abi.ts
@@ -0,0 +1,21 @@
+export const erc20Abi = [
+    {
+        "constant": true,
+        "inputs": [{"name": "_owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "", "type": "uint256"}],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [{"name": "", "type": "uint256"}],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    
+];
